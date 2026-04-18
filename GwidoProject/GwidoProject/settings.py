bf10b635cfb3f8dd72dca14c1f41250436944aed
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2026-04-18 23:52:13 +0200

    Added authentication

--- GwidoProject/GwidoProject/settings.py
+++ GwidoProject/GwidoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "village_banking",
+    "auth_app",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "GwidoProject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
