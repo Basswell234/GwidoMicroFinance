0f86f6afa7b921516ee1518d1689a8e42164127a
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2026-04-25 08:16:16 +0200

    Added model

--- GwidoProject/village_banking/models.py
+++ GwidoProject/village_banking/models.py
@@ -29,3 +29,25 @@ class Person(models.Model):
     
     class Meta:
         ordering = ['lastname', 'firstname']
+
+
+class Membership(models.Model):
+    ROLE_CHOICES = [
+        ('member', 'Member'),
+        ('non-member', 'Non-Member'),
+        ('treasurer', 'Treasurer'),
+        ('secretary', 'Secretary'),
+        ('admin', 'Admin'),
+    ]
+    
+    membership_id = models.AutoField(primary_key=True)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    join_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    
+    def __str__(self):
+        return f"{self.person_id.firstname} - {self.category}"
+    
+    class Meta:
+        ordering = ['-join_date']
