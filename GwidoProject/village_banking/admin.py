a08940f375ae4d67e636ee7dd0c6600ccb416c94
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2026-04-25 09:11:04 +0200

    registered all the models created

--- GwidoProject/village_banking/admin.py
+++ GwidoProject/village_banking/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register([Person, Membership, Investment, Transaction, Loan, Penalty, Interest, Loan_Payment])
+
