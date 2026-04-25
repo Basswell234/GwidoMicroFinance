9cd3c640843f492016546bbbdb9911eec8912123
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2026-04-25 08:01:50 +0200

    Added model

--- GwidoProject/village_banking/models.py
+++ GwidoProject/village_banking/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    
+    MEMBERSHIP_STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('suspended', 'Suspended'),
+    ]
+    
+    person_id = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255)
+    occupation = models.CharField(max_length=100)
+    membership_status = models.CharField(max_length=20, choices=MEMBERSHIP_STATUS_CHOICES)
+    membership_desc = models.TextField(blank=True, null=True)
+    nrc = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+    
+    class Meta:
+        ordering = ['lastname', 'firstname']
